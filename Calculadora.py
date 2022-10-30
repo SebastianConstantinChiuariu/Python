3a51e53070ffd7fbe7e6ab9501937f491d96c9a4
SebastianConstantinChiuariu <116726403+SebastianConstantinChiuariu@users.noreply.github.com>
2022-10-30 11:14:26 +0100

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,23 @@
+from unittest import case
+
+from numpy import rint
+
+
+print("    \n        _Calculadora.py_\n")
+print("Escull la opció que necesitis:\n\n")
+print("1.Càlculs Aritmètics\n")
+print("2.Càlcul Potències\n")
+print("3.Sortir\n")
+a = 1
+a = input("Selecció:")
+
+if a : 1
+print("\n\n""_Càlculs Aritmètics_\n\n")
+b = input("Introdueix primera dada:")
+c = input("Càlcul que és fara:")
+d = input("Introdueix segona dada:")
+e = ("Resultat")
+c=="+"
+print("\n" "La dada ",b," + ",d," es igual a: ",b+d,)
+    
+
---
+++ Pou.OS.py
@@ -0,0 +1,77 @@
+"\n"
+from os import system
+from time import sleep
+
+
+print("\n""         Pou.OS\n")
+a = input("Intordueix nom d'usuari: ")
+b = "Dequa20."
+b = input("\nConstrasenya: ")
+system('clear')
+if b == "Dequa20.":  
+    print("\n""    Executant Pou.OS\n\n")
+    sleep(2)
+    system('clear')
+    sleep(1.5)
+    print("\n""<Preparant l'Escriptori>")
+    sleep(1.5)
+    system('clear')
+    sleep(2.5)
+    system('clear')
+elif b >= "":
+    print("\n""Contrasenya Incorrecta""\n")
+    c = "Dequa20."
+    c = input(" Intenta-ho de nou:")
+    if c == "Dequa20.":
+        print("\n""   Exexutant Pou.OS")
+        sleep(2)
+        print("\n""          Hola " + a)
+        system('clear')
+    elif c >= "":
+            print("\n""  Usuari Bloquetjat")
+            print("\n""   Reincia l'equip")
+
+sleep(0.5)
+print("\n""Usuari: " + a)
+sleep(1)
+print("\n""Menú""\n")
+sleep(1.5)
+print("1.Calculadora""\n")
+sleep(0.5)
+print("2.Document Text""\n")
+sleep(0.5)
+d = input("Executar: ")
+if d == "1":
+    system('clear')
+    sleep(1)
+    print("\n""Carregant")
+    sleep(1)
+    system('clear')
+    print("\n""Carregant.") 
+    sleep(1)
+    system('clear')
+    print("\n""Carregant..")
+    sleep(1)
+    system('clear')
+    print("\n""Carregant...")
+    sleep(2)
+    system('clear')
+    print("\n""Calculadora Aritmètica""\n\n")
+
+n1 = input("Insereix una xifra: ")
+system('clear')
+sleep(1)
+print("\n""   Càlculs Disponibles""\n")
+print("(+).Sumar""\n")
+print("(-).Restar""\n")
+print("(*).Multiplicar""\n")
+print("(/).Dividir""\n")
+print("(**),Potènciar""\n\n")
+n2 = input("Selecciona el operant: ")
+system('clear')
+sleep(0.5)
+n3 = input("\n""Insereix altre xifra: ")
+if n2 == "+":
+    print("La suma de ",n1," + ",n3," es igual a = ")
+
+
