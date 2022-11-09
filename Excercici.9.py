ac3e2c90a780b43ae2e55f40a2103fa59b7237f8
SebastianConstantinChiuariu <116726403+SebastianConstantinChiuariu@users.noreply.github.com>
2022-11-09 20:24:37 +0100

    Add files via upload

---
+++ Excercici.9.py
@@ -0,0 +1,9 @@
+from os import system
+
+
+print("     Excercici 9""\n")
+a = input("Introdueix el teu Nom: ")
+b = input("Introdueix el teu Cognom: ")
+system('clear')
+print("\n""Bon dia,",a,b,"")
+
---
+++ Excercici10.py
@@ -0,0 +1,7 @@
+a = input("Introdueix la teva edat: ")
+if (int(a)>18):
+    print(a,",ets major de 18 anys")
+elif (int(a)<18):
+    print(a, ",ets menor de 18 anys")
+else:
+    print(a, "," "Tens 18 anys!!")
