c6427e3cfe870c047693dc1cbaa2e1cebf2af256
SebastianConstantinChiuariu <116726403+SebastianConstantinChiuariu@users.noreply.github.com>
2022-11-24 10:18:14 +0100

    Add files via upload

---
+++ Excercici12.py
@@ -0,0 +1,16 @@
+def gran_de_tres (a,b,c):
+    a=a
+    if (a>b):
+        a = a
+    elif (b>a):
+        a = b
+    else: 
+        a = a 
+    
+
+
+    if (a<b):
+        print(a, "Es menor a",b,)
+
+
+    
