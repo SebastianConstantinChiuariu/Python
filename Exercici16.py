c982df45222a1350b669d1bfda3fb8e899ef596d
SebastianConstantinChiuariu <116726403+SebastianConstantinChiuariu@users.noreply.github.com>
2022-11-30 12:05:05 +0100

    Add files via upload

---
+++ Exercici16.py
@@ -0,0 +1,18 @@
+from time import sleep
+from os import system
+system('clear')
+
+#menú principal
+
+print("\n           Inversor de textos")
+
+def invertir (text):
+    caracters = "aba"
+
+    for i in caracters in text [-999] - [0]:
+        return text 
+
+    print(invertir(text))
+
+caracters = input("\n\nEscriu el text: ")
+print(invertir())
