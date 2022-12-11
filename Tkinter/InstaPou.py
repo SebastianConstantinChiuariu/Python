5d1c841a1b6e21394bff143291211312a7037fdc
SebastianConstantinChiuariu <116726403+SebastianConstantinChiuariu@users.noreply.github.com>
2022-12-11 22:22:43 +0100

    Add files via upload

--- Tkinter/InstaPou.py
+++ Tkinter/InstaPou.py
@@ -1,4 +1,18 @@
+import customtkinter
 import tkinter
 
 
-tkinter.set_appearance_mode("dark")
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+root.geometry("500x440")
+
+def login():
+    print("Username")
+
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx = 60, fill="both", expand=True("In english please"))
+
+root.mainloop()
