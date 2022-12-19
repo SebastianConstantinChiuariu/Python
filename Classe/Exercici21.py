623429200beafde784d9dfc67f8faf6437b26aa4
SebastianConstantinChiuariu <116726403+SebastianConstantinChiuariu@users.noreply.github.com>
2022-12-19 13:22:53 +0100

    Add files via upload

--- Classe/Exercici21.py
+++ Classe/Exercici21.py
@@ -3,6 +3,116 @@ from time import sleep
 
 #Menú 
 
-print("Dibiux per la pantalla")
-def pixels(img):
+opcio = 1
+while(opcio!=0):
+    print("\n                   Dibuix a la pantalla")
+    print("""\n 
+                        0. Sortir
+                        1. Rombo
+                        2. Triangle
+                        3. Shuerk
+                        4. Pato
+    _________________________________________________""")
+    #Programa
+    a = input("\n                          Dibuix: ")
+    match a:
+            case "0":
+                system('cls')
+                sleep(1)
+                print("\n          ||   SYSTEM  OFF   ||")
+                off = input ("\n\n            ENTER PER INICIAR ")
+                system('cls')
+                print("\n\n\n                 ||")
+                sleep(0.6)
+                system('cls')
+                print("\n\n\n                 | . |")
+                sleep(0.6)
+                system('cls')
+                print("\n\n\n                 | .. |")
+                sleep(0.6)
+                system('cls')
+                print("\n\n\n                 | ... |")
+                sleep(0.6)
+                system('cls')
+                print("\n\n\n                 | .... |")
+                sleep(1)
+                system('cls')
+                opcio = 1
+
+            case "1":
+                system('cls')
+                sleep(1)
+                print("\n\n                    Dibuix d'un Rombo ")
+                sleep(1)
+                print("""
+                            .
+                        .       .
+                    .               .
+                 .                     .
+                    .               .
+                        .       .
+                            .
+                """)
+                sleep(1)
+                repetir = input("\n\n                        Repetir: ")
+
+            case "2":
+                system('cls')
+                sleep(1)
+                print("\n\n                    Dibuix d'un Triangle ")
+                sleep(1)
+                print("""
+                             .
+                         .       .
+                     .               .
+                  .                     .
+                .  .  .  .  .  .  .  .  .  .    
+                """)
+
+            case "3":
+                system('cls')
+                sleep(1)
+                print("\n\n                    Dibuix d'un Triangle ")
+                sleep(1)
+                print("""
+                
+                ⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+                ⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+                ⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀ 
+                ⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀ 
+                ⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆ 
+                ⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿ 
+                ⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀ 
+                ⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+                ⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+                ⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+                ⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ 
+                ⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀ 
+                ⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀ <
+                ⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉
+                """)
+
+            case "4":
+                system('cls')
+                sleep(1)
+                print("\n\n                    Dibuix d'un Triangle ")
+                sleep(1)
+                print("""
+                   _      _      _
+                 >(.)__ <(.)__ =(.)__
+                  (___/  (___/  (___/  
+                _______________________
+
+                          w_
+                        <(o )___
+                          ( ._> /
+                           `---'
+                """)
+
+
+
+                
+                
+
     
